9e7ba2f991d9014cb0e3db3c77276d77195977d9
emileNetter <emile@netter.fr>
2016-11-29 10:23:30 +0100

    First commit

---
+++ genereEch.m
@@ -0,0 +1,23 @@
+function [x] = genereEch (N,fe,f0,N_rep)
+N_v = 0:N-1;
+x=sin(2*pi*f0/fe*N_v);
+Nfft = 4096;
+X=fft(x,Nfft);
+
+%représentation
+axe_f=-fe/2:fe/Nfft:fe/2-fe/Nfft;
+
+figure, 
+subplot 211
+%représentation temporelle
+plot(N_v,x);
+xlim([0 N_rep-1]); %limite valeurs de x
+xlabel('Numéro échantillon');
+ylabel('s(t)');
+
+subplot 212
+%représentation en fréquence
+plot(axe_f,fftshift(abs(X)));
+title('f(Hz)');
+
+end
---
+++ genereEchantillons.m
@@ -0,0 +1,10 @@
+clear global;
+close all;
+
+%%Question 1 
+
+x=genereEch(11025,44100,880,1000);
+soundsc(x,fe+100000);
+
+
+
