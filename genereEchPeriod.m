c6d8c7a1ea300fb5d19be2f1f9e21c8ec402d5fd
emileNetter <emile@netter.fr>
2016-11-29 11:00:54 +0100

    Genere EchPeriod modifs

--- genereEchPeriod.m
+++ genereEchPeriod.m
@@ -1,13 +1,10 @@
-function [ note ] = genereEchPeriod( N,fe,f0,nbh )
+function [ note ] = genereEchPeriod(note, N,fe,f0,nbh )
 N_v = 0:N-1;
-note = sin(2*pi*f0/fe*N_v);
 amp_h = [1,0.7,0.95,0.75,0.6,0.65,0.65,0.5,...
     0.6,0.5,0.45,0.5,0.6,0.5,0.25,0.5,0.3,0.25,0.3,0.4,0.3,0.25,0.2,0.15];
 
 for i =1:nbh
     note = note +amp_h(i)*sin(2*pi*(i+1)*f0/fe*N_v);
-
-
 
 end
 
--- genereEchantillons.m
+++ genereEchantillons.m
@@ -6,6 +6,7 @@ N= 11025;
 fe=44100;
 f0=880;
 N_rep=1000;
+N_v=0:N-1;
 fct=sin(2*pi*f0/fe*N_v);
 
 x=genereEch(fct,N,fe,f0,N_rep);
@@ -13,13 +14,13 @@ soundsc(x,fe+100000);
 
 %%
 nbh=4;
-note = genereEchPeriod(N,fe,f0,nbh);
+note = genereEchPeriod(fct,N,fe,f0,nbh);
 genereEch(note,N,fe,f0,N_rep);
 soundsc(note,fe);
 
 %%
 nbh=0;
-note2=genereEchPeriod(N,fe,f0,nbh);
+note2=genereEchPeriod(fct,N,fe,f0,nbh);
 genereEch(note2,N,fe,f0,N_rep);
 soundsc(note2,fe);
 
